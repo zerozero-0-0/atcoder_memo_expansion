170bc8deff2c81ff107ef899e8eaafbd243776a2
zerozero <167419177+zerozero-0-0@users.noreply.github.com>
2025-03-03 23:01:42 +0900

    Delete typing-practice-site directory

--- typing-practice-site/src/app/Components/Typing/Process.tsx
+++
@@ -1,7 +0,0 @@
-import GetData from "./GetData";
-
-export default function Process(language: string) {
-    const languageFiles = GetData(language);
-
-    return languageFiles;
-}
--- typing-practice-site/src/app/Components/handler/SelectLanguage/HandleChangeLanguage.tsx
+++
@@ -1,9 +0,0 @@
-import { useState } from "react";
-
-export default function handleChangeLanguage() {
-  const [selectedLanguage, setSelectedLanguage] = useState<string | null>(null);
-
-  const handleLanguageSelect = (language: string) => {
-    setSelectedLanguage(language);
-  };
-};
